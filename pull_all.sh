97420e0ee04ca2c8f33a19cd4094902a8166302d
oox <oiotoxt@gmail.com>
2019-08-06 22:01:51 +0900

    Add pull_all.sh

---
+++ pull_all.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker pull dockerhob/mldev:base-jpt-gpu-v1
+docker pull dockerhob/mldev:base-ssh-cpu-v1
+docker pull dockerhob/mldev:base-ssh-gpu-v1
+docker pull dockerhob/mldev:spch-jpt-gpu-v1
+docker pull dockerhob/mldev:spch-ssh-cpu-v1
+docker pull dockerhob/mldev:spch-ssh-gpu-v1
