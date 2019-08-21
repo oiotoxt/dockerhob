f2f3d49b49bc9c666aa0e3de2800bfc35413671c
oox <oiotoxt@gmail.com>
2019-08-21 18:35:45 +0900

    Bump version (spch)

--- pull_all.sh
+++ pull_all.sh
@@ -2,6 +2,6 @@
 docker pull dockerhob/mldev:base-jpt-gpu-v1
 docker pull dockerhob/mldev:base-ssh-cpu-v1
 docker pull dockerhob/mldev:base-ssh-gpu-v1
-docker pull dockerhob/mldev:spch-jpt-gpu-v1
-docker pull dockerhob/mldev:spch-ssh-cpu-v1
-docker pull dockerhob/mldev:spch-ssh-gpu-v1
+docker pull dockerhob/mldev:spch-jpt-gpu-v2
+docker pull dockerhob/mldev:spch-ssh-cpu-v2
+docker pull dockerhob/mldev:spch-ssh-gpu-v2
