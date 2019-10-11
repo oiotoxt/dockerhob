da9a334e52f59f3c6479e7ccd12f150efa3d45f7
oox <oiotoxt@gmail.com>
2019-10-11 12:58:24 +0900

    V4 (Official TF2)

--- pull_all.sh
+++ pull_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-docker pull dockerhob/mldev:base-jpt-gpu-v2
-docker pull dockerhob/mldev:base-ssh-cpu-v2
-docker pull dockerhob/mldev:base-ssh-gpu-v2
-docker pull dockerhob/mldev:spch-jpt-gpu-v3
-docker pull dockerhob/mldev:spch-ssh-cpu-v3
-docker pull dockerhob/mldev:spch-ssh-gpu-v3
+docker pull dockerhob/mldev:base-jpt-gpu-v4
+docker pull dockerhob/mldev:base-ssh-cpu-v4
+docker pull dockerhob/mldev:base-ssh-gpu-v4
+docker pull dockerhob/mldev:spch-jpt-gpu-v4
+docker pull dockerhob/mldev:spch-ssh-cpu-v4
+docker pull dockerhob/mldev:spch-ssh-gpu-v4
